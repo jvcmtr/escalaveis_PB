e0f7bf99c8f98c9750923cc16125475b191cbf21
jvcmtr <jvictorcmtr@gmail.com>
2025-11-16 19:52:27 -0300

    Added models

---
+++ Joao_Ramos_PB_TP1/Joao_Ramos_PB_TP1/src/main/java/edu/infnet/Joao_Ramos_PB_TP1/models/Issues/EIssueStatus.java
@@ -0,0 +1,8 @@
+package edu.infnet.Joao_Ramos_PB_TP1.models.Issues;
+
+public enum EIssueStatus {
+    OPEN,
+    IN_PROGRESS,
+    RESOLVED,
+    CLOSED
+}
---
+++ Joao_Ramos_PB_TP1/Joao_Ramos_PB_TP1/src/main/java/edu/infnet/Joao_Ramos_PB_TP1/models/Issues/Issue.java
@@ -0,0 +1,27 @@
+package edu.infnet.Joao_Ramos_PB_TP1.models.Issues;
+
+import edu.infnet.Joao_Ramos_PB_TP1.models.base.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Issue extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private EIssueStatus status;
+}
