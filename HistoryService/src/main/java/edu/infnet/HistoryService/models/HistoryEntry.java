d00f116c5f26dab9da5933bdfc04b8e2717703ec
jvcmtr <jvictorcmtr@gmail.com>
2025-11-23 05:17:18 -0300

    minor fixes

--- HistoryService/src/main/java/edu/infnet/HistoryService/models/HistoryEntry.java
+++ HistoryService/src/main/java/edu/infnet/HistoryService/models/HistoryEntry.java
@@ -2,6 +2,7 @@ package edu.infnet.HistoryService.models;
 
 import java.time.ZonedDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +23,10 @@ public class HistoryEntry {
     private String username;
     private String action;
     private ZonedDateTime date;
+    
+    @Column(length = 2000)
     private String oldStateJSON;
+    
+    @Column(length = 2000)
     private String newStateJSON;
 }
